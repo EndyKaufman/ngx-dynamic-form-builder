63b64455692898f9e5bf824dd76824cc45c09cdf
EndyKaufman <admin@site15.ru>
2020-09-21 12:50:28 +0400

    fix: update detect logic object property value in ExposeNested

--- libs/ngx-dynamic-form-builder/src/lib/decorators/expose-nested.decorator.ts
+++ libs/ngx-dynamic-form-builder/src/lib/decorators/expose-nested.decorator.ts
@@ -2,11 +2,11 @@ import { Expose, ExposeOptions, Transform } from 'class-transformer';
 
 // for correct work expose with js object
 // https://github.com/typestack/class-transformer/issues/365
-export function ExposeNested(options?: ExposeOptions) {
+export function ExposeNested(options?: ExposeOptions & { default: any }) {
   const exposeFn = Expose(options);
   const transformFn = (propertyKey: string) =>
     Transform((value: any, obj: any) => {
-      return obj ? obj[propertyKey] : undefined;
+      return (obj && Object.getOwnPropertyDescriptor(obj, propertyKey) ? obj[propertyKey] : options?.default) || value;
     });
   return (target: any, propertyKey: string) => {
     transformFn(propertyKey)(target, propertyKey);
